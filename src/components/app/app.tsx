8e6c83651d9f8d31ce6381a35d14b8c8db803a2f
Maria Avanova <mariaavanova@192.168.1.3>
2023-11-23 22:28:49 +0300

    first lesson

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import Header from '../header';
+
+const App = () => {
+	return (
+		<>
+			<Header /> HomePage Footer
+		</>
+	);
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
-import { StrictMode } from 'react';
+import App from './components/app';
+import './global.css';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
+
 root.render(
 	<StrictMode>
 		<App />
